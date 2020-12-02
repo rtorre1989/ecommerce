15b31ec46590cffbf565806bcb7b6b6c0a96a841
rtorre1989 <rodrigotorre@msn.com>
2020-12-02 13:06:15 -0300

    Add files via upload

---
+++ backend/src/main/java/br/com/ecommerce/backend/controller/PedidosController.java
@@ -0,0 +1,17 @@
+package br.com.ecommerce.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("pedidos")
+public class PedidosController {
+
+    @GetMapping(value = "teste")
+    public String teste() {
+        return "teste";
+    }
+
+}
